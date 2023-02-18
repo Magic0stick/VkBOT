569523b6ffd46a9bfa8802fd798df17929b9531a
Magic0stick <77047717+Magic0stick@users.noreply.github.com>
2023-02-18 07:17:14 +0300

    1.0
    1.0 relise

--- bot.py
+++ bot.py
@@ -4,9 +4,6 @@ import requests## импорт библиотеки запросов
 from bs4 import BeautifulSoup as bs## импорт библиотеки для разбора 
 from scr import *
 
-
-
-
 def found(b,a): ## фунция поиска
     URL_TEMPLATE = b + a ##переменная запроса
     URL_TEMPLATE = URL_TEMPLATE.replace(" ", "") #очистка от лишних пробелов
@@ -18,16 +15,32 @@ def found(b,a): ## фунция поиска
         data = (data[0:4093] + '...')
     return data
 
+def translateen(b,a):
+    URL_TEMPLATE = b + a ##переменная запроса
+    URL_TEMPLATE = URL_TEMPLATE.replace(" ", "") #очистка от лишних пробелов
+    r = requests.get(URL_TEMPLATE) #переменная запроса с сервера
+    soup = bs(r.text, "html.parser") #переменная анализа информации
+    wikiinfo = soup.find('div', class_="ru_title") #поиск информации и задача переменной
+    data = wikiinfo.text
+    if len(data) > 4093:
+        data = (data[0:4093] + '...')
+    return data
 
-
+def translatefr(b,a):
+    URL_TEMPLATE = b + a ##переменная запроса
+    URL_TEMPLATE = URL_TEMPLATE.replace(" ", "") #очистка от лишних пробелов
+    r = requests.get(URL_TEMPLATE) #переменная запроса с сервера
+    soup = bs(r.text, "html.parser") #переменная анализа информации
+    wikiinfo = soup.find("p",class_="truncate") #поиск информации и задача переменной
+    data = wikiinfo.text
+    data = data[2:]
+    if len(data) > 4093:
+        data = (data[0:4093] + '...')
+    return data
 
 def write_msg(user_id, message): #функция отправки сообщения
     print(message)
     vk.method('messages.send', {'user_id': user_id, 'message': message,"random_id":vk_api.utils.get_random_id()})
-
-
-
-
 
 # Авторизуемся как сообщество
 vk = vk_api.VkApi(token=token)
@@ -51,4 +64,6 @@ for event in longpoll.listen():
                 break
             request = request.lower()
             
+            write_msg(event.user_id,translateen(SlovarEN,request))
+            write_msg(event.user_id,translatefr(SlovarFR,request))
             write_msg(event.user_id,found(SlovarRU,request))
--- scr.py
+++ scr.py
@@ -2,6 +2,5 @@
 token = "vk1.a.JVVOtUe3eVQECb_SFzuuv1e0ocCGtkoHinEkYriehka5WLrf5bNURB9CmQ4ii29VB4K252BzT8ye1AS0Y8QtTRtiIdHngbLNkHr24rFxtQw7NAlHLRxIE7jymKLRsPxWXDQQIEzeNmIddd4xoy3MWTY8Tfk3ZYjFHl-29MiH3k4ve3ZOLulIHxNNfEO4UH0_xbMPpJEwzUntMbvKmhHJZw"
 
 SlovarRU = "https://ozhegov.info/slovar/?q="
-SlovarEN = "https://translate.yandex.ru/?source_lang=ru&target_lang=en&text="
-SlovarFR = "https://translate.yandex.ru/?source_lang=ru&target_lang=fr&text="
-
+SlovarEN = "https://wooordhunt.ru/word/"
+SlovarFR = "https://big-fr-rus-fr-dict.slovaronline.com/search?s="
